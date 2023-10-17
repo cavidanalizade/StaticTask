a9fbb41406877bb54a480053965914d4e6efa05f
cavidanalizade <145671273+cavidanalizade@users.noreply.github.com>
2023-10-18 01:33:31 +0400

    Add files via upload

---
+++ StaticTask/StaticTask/ExtensionMethods.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticTask
+{
+    internal static class ExtensionMethods
+    {
+        public static bool CustomContains(this string extension , string searchString)
+        {
+            return extension.ToLower().Contains(searchString.ToLower());
+
+        }
+        public static bool CustomContains(this string extension, char searchChar)
+        {
+            return extension.ToLower().Contains(searchChar.ToString().ToLower());
+
+        }
+
+        public static bool IsPrime(this int extension)
+        {
+            if (extension <= 1)
+                return false;
+
+            for (int i = 2; i * i <= extension; i++)
+            {
+                if (extension % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsPowOfTwo(this int extension)
+        {
+            if (extension <= 0)
+                return false;
+
+ 
+            return (extension & (extension - 1)) == 0;
+
+        }
+
+
+    }
+}
---
+++ StaticTask/StaticTask/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace StaticTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Javidan";
+            Console.WriteLine(a.CustomContains("ja"));
+            Console.WriteLine(a.CustomContains('j'));
+            int number = 32;
+            Console.WriteLine(number.IsPrime());
+            Console.WriteLine(number.IsPowOfTwo());
+        }
+    }
+}
